34d5cd07e39a2343566b575491495849a4be72c5
David <contato.davidalmeida@outlook.com>
2023-12-03 09:18:32 -0300

    🤡 Mock things

---
+++ src/mock/menuOptions.ts
@@ -0,0 +1,34 @@
+import { Option } from "@/interfaces/Option";
+
+export const menu: Option[] = [
+  {
+    id: 1,
+    url: "/",
+    name: "Home",
+    arialabel: "home page",
+  },
+  {
+    id: 2,
+    url: "/game-info",
+    name: "Game info",
+    arialabel: "game information",
+  },
+  {
+    id: 3,
+    url: "/gallery",
+    name: "Gallery",
+    arialabel: "image gallery",
+  },
+  {
+    id: 4,
+    url: "/guides",
+    name: "Guides",
+    arialabel: "guides",
+  },
+  {
+    id: 5,
+    url: "/about",
+    name: "About",
+    arialabel: "about the game",
+  },
+];
