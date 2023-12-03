fe9fa6ae567c493a6372dcb08e10b142d945ce4f
David <contato.davidalmeida@outlook.com>
2023-12-03 10:07:04 -0300

    🔧 Add or update configuration files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		colors: {
+			green: "#448441",
+			red: "#cd2e2f",
+			white: "#ffffff",
+			black: "#333333",
+			lightGray: "#e0dfe1",
+			disabledGray: "#7e8085",
+		},
+		extend: {
+			backgroundImage: {
+				"hero-pattern": "url(../assets/bg-metin2.webp)",
+			},
+		},
+	},
+	plugins: [],
+};
+
+export default config;
