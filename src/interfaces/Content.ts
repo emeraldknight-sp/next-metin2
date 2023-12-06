03d075f2d01879e7f88af77002f7fed67f11603d
David <contato.davidalmeida@outlook.com>
2023-12-06 11:49:03 -0300

    ✅ Add: Content mock data and interface

---
+++ src/interfaces/Content.ts
@@ -0,0 +1,3 @@
+export interface Content {
+	text: string | string[];
+}
---
+++ src/mock/contents.ts
@@ -0,0 +1,20 @@
+import { Content } from "@/interfaces/Content";
+
+export const contents: Content[] = [
+	{
+		text: [
+			`For a long time now the Dragon God's breath has watched over the kingdoms of Shinsoo, Chunjo and Jinno.`,
+			`But fascinating world of magic is facing a terrifying threat: The impact of the Metin Stones has not only torn wounds into the entire continent, but has also caused chaos and destruction throughout the land and its inhabitants.`,
+		],
+	},
+	{
+		text: [
+			`The story of Metin2 takes place in a fantasy world inspired by Chinese mythology. Players can choose between different kingdoms and character classes, each with their own abilities and play styles. The central plot involves the propagation of mysterious stones called "Metins", which bring incredible power but also cause destruction and chaos.`,
+		],
+	},
+	{
+		text: [
+			`Players are challenged to explore the world, face monsters, complete quests, develop their skills and interact with other players. Additionally, Metin2 emphasizes competition between players, including inter-kingdom battles and guild wars.`,
+		],
+	},
+];
